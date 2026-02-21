4082973ca5ac79249042cae64eda3364fa6d6de0
aker777 <72918521+aker777@users.noreply.github.com>
2026-02-21 19:25:06 +0100

    Fix auth storage for native

--- moonsoon-app/utils/supabase.ts
+++ moonsoon-app/utils/supabase.ts
@@ -1,12 +1,19 @@
-import 'expo-sqlite/localStorage/install';
+import { Platform } from 'react-native';
 import { createClient } from '@supabase/supabase-js';
+import * as SecureStore from 'expo-secure-store';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => SecureStore.getItemAsync(key),
+  setItem: (key: string, value: string) => SecureStore.setItemAsync(key, value),
+  removeItem: (key: string) => SecureStore.deleteItemAsync(key),
+};
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
 const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
   auth: {
-    storage: localStorage,
+    storage: Platform.OS === 'web' ? localStorage : ExpoSecureStoreAdapter,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
